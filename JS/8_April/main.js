dfea35995be6c0d3ad20c3f2714fb1c2eb293d8c
itx-mj65 <jamshedweb65@gmail.com>
2025-04-11 09:18:15 +0500

    10 of march

---
+++ JS/8_April/main.js
@@ -0,0 +1,39 @@
+
+let Abc = BigInt(659776869869);
+// let Abc = 65975698468689698698n; 
+Abc = null
+Abc = ["string", "second", 5687346, true];
+Abc = {
+    name: "faizan",
+    age: 25,
+    faizan: ["web dev", "nill", true]
+}
+
+
+console.log(Abc.name + "  with array " + Abc.faizan[0])
+//  Abc= "this is overriding" 
+// let Abc = "redec value";  // redec
+
+let b = 5;
+b--
+let btx = b
+//  var b="over the b value"; // "we can overide and also redec a var variable"
+document.write("<h1>" + btx + "</h1>")
+
+const c = "this is const";
+// const c="over the const value"; // can't derec or override a const value 
+console.log(c);
+
+
+document.write(Abc);
+console.log(Abc)
+alert(Abc)
+
+
+{
+    var x = "in a block 8758";
+
+}
+
+console.log(x);
+
---
+++ JS/9_April/main.js
@@ -0,0 +1,50 @@
+let arr = ["orange", "apple", "mango", "kiwi"];
+// console.log(arr.length); 
+// console.log(arr.toString());
+// console.log(arr[3]);
+//  document.write(arr.at(3))
+// console.log(arr.at(2));
+//  document.write(arr.join(" % "))
+// arr.pop()
+// arr.push("banana")
+
+// arr.shift()
+// arr.unshift("value")
+
+
+// console.log(arr[3])
+//  arr.pop();
+ delete arr[2]
+
+console.log(arr)
+
+
+let arr1 = ["mango", "orange", "kiwi", "value 1", "value 2"];
+let arr2 = ["carrot", "potato", "onion"]
+
+// let newarr=arr1.concat(arr2);
+// console.log(newarr)
+
+// let newarr = arr1.copyWithin(2,0);
+// console.log(newarr)
+
+const myArr = [[1,2],[3,4],[5,6]];
+let flatarr=myArr.flat()
+console.log(flatarr)
+
+let arr3 = [ "value 1", "value 2", "value 3", "value 4"];
+
+let newarr3=arr3.splice(1,2,"mango", "orange", "kiwi",)
+console.log(newarr3 + " new array") 
+console.log(arr3 + " orignal array") 
+// console.log(newarr3)
+
+const months = ["Jan", "Feb", "Mar", "Apr"];
+const spliced = months.toSpliced(0, 1);
+console.log(spliced + " new array");
+console.log(months + " orignanl array ");
+
+
+
+
+
