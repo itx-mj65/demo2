658ca5335f2d59af610b3f857ebc653bedc71a58
itx-mj65 <jamshedweb65@gmail.com>
2025-04-15 16:47:33 +0500

    15 of April

---
+++ JS/14_April/main.js
@@ -0,0 +1,33 @@
+// let arr =["apple", true , 67984, null , "value 2 "];
+// console.log(arr.length) 
+// console.log(arr.toString())
+// console.log(arr.at(3))
+// console.log(arr[2])
+// document.write(arr.join(" & "));
+// arr.unshift("value add")
+// console.log(arr)
+
+let a = 2;
+
+// switch (a) {
+//     case 25:
+//         console.log("value is 25");
+//         break;
+
+//     case 27:
+//         console.log("value is 27");
+//         break;
+//     default:
+//         console.log("value is not 25 or 27");
+
+
+// }
+ let num = 10
+
+for(let i = 0 ; i<=10 ; i++ ){
+    console.log("this is " + i)
+}
+
+
+
+
